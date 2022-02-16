2a7664364e1d08d6e7997d8d6a419fe8bb699c38
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-15 19:56:13 -0500

    Operadores Asignacion

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-//Ventana emergente en JS
-let nombre = prompt(`Ingrese el nombre del usuario`, `Usuario ingrese su nombre porfavor`);
-console.log(nombre);
+// Operadores Asignacion
+let x;
+let y = 50;
+let myStyle = `
+    color: #fff;
+    background: #226666;
+    padding: 5px;
+    border-radius: 5px;
+`;
+
+// Asignacion
+console.group(`%cOperadores Asignacion`, myStyle);
+
+
+    console.log(`Variable x: ${x} Variable y: ${y}`);
+
+    console.log(`Abreviacion: x = y;`);
+    // Abreviacion
+    x = y;
+    console.log(`x: ${x} y: ${y}`);
+
+
+
+console.groupEnd();
