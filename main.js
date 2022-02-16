e4ae4b2cfae00192789f2111a1740804bcc6508f
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-15 21:08:25 -0500

    Operadores Arigmetico Suma

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-// Operadores Adición
-let x = null;
-let y = 50;
+// Operadores Arigmetico Suma
+let num1 = parseInt(prompt("Usuario ingrese el numero 1", 1));
+let num2 = new Number(prompt("Usuario ingrese el numero 2", 2));
 let myStyle = `
     color: #fff;
-    background: #801515801515;
+    background: #801515;
     padding: 5px;
     border-radius: 5px;
 `;
-// Adición
-console.group(`%cOperadores Adición`, myStyle);
 
+console.group(`%cOperadores Arigmetico Suma`, myStyle);
 
-    console.log(`Variable x: ${x} Variable y: ${y}`);
-
-    // Abreviacion
-    console.log(`Abreviacion: x += y;`);
-    x += y;
-
-    // // Significado
-    // console.log(`Significado: x = x + y;`);
-    // x = x + y;
-
-    console.log(`x: ${x} y: ${y}`);
-
-
+    console.log(`Variable num1: ${num1} num2: ${num2}`);
+    num1 += num2;
+    console.log(`La suma es: ${num1}`);
 
 console.groupEnd();
