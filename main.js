5ade97e780b30fe8299bc428513862f5eb3249d7
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-24 19:21:40 -0500

    Operadores Arigmetico Exponenete

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Operadores Arigmetico Modulo
+// Operadores Arigmetico Exponenete
 let num1 = parseInt(prompt("Usuario Ingrese un numero mayor que 0", 1));
 let num2 = new Number(prompt("Usuario ingrese siguiente numero", 1));
 let myStyle = `
@@ -8,11 +8,11 @@ let myStyle = `
     border-radius: 5px;
 `;
 
-console.group(`%cOperadores Arigmetico Modulo`, myStyle);
+console.group(`%cOperadores Arigmetico Exponenete`, myStyle);
 
     console.log(`Variable num1: ${num1} num2: ${num2}`);
-    //Operacion de Modulo
-    num1 %= num2;
-    console.log(`La Modulo es: ${num1}`);
+    //Operacion de Exponenete
+    num1 **= num2;
+    console.log(`La Exponenete es: ${num1}`);
 
 console.groupEnd();
