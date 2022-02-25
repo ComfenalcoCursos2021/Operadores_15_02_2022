7b63f9bbde159d59930f0244333113fbea9ad8d6
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-24 19:39:18 -0500

    Piramide de prioridades

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-// Operadores Arigmetico Exponenete
-let num1 = parseInt(prompt("Usuario Ingrese un numero mayor que 0", 1));
-let num2 = new Number(prompt("Usuario ingrese siguiente numero", 1));
-let myStyle = `
-    color: #fff;
-    background: #567714;
-    padding: 5px;
-    border-radius: 5px;
-`;
+let reslutado  = 5*((4/4)*(9-7)*2)+6;
+//  5*((1)*(9-7)*2)+6
+//  5*((1)*(2)*2)+6
+//  5*(2*2)+6
+//  5*(4)+6
+//  5*(4)+6
+//  20+6
+//  26
+console.log(reslutado);
 
-console.group(`%cOperadores Arigmetico Exponenete`, myStyle);
-
-    console.log(`Variable num1: ${num1} num2: ${num2}`);
-    //Operacion de Exponenete
-    num1 **= num2;
-    console.log(`La Exponenete es: ${num1}`);
-
-console.groupEnd();
+let reslutado2  = 5+1*8+(9 * (5+8) / 2);
+//  5+1*8+(9*(5+8)/2)
+//  5+1*8+(9*(13)/2)
+//  5+1*8+(117/2)
+//  5+1*8+(58.5)
+//  5+ 8 +58.5
+//  13 + 58.5
+//  71.5
+console.log(reslutado2);
