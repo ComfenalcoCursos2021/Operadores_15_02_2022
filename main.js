19c64bc2938e587f95caa4723108a55faa711ee7
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-24 19:18:48 -0500

    Operadores Arigmetico Multiplicacion

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Operadores Arigmetico Resta
+// Operadores Arigmetico Multiplicacion
 let num1 = parseInt(prompt("Usuario Ingrese un numero mayor que 0", 1));
 let num2 = new Number(prompt("Usuario ingrese siguiente numero", 1));
 let myStyle = `
@@ -8,11 +8,11 @@ let myStyle = `
     border-radius: 5px;
 `;
 
-console.group(`%cOperadores Arigmetico Resta`, myStyle);
+console.group(`%cOperadores Arigmetico Multiplicacion`, myStyle);
 
     console.log(`Variable num1: ${num1} num2: ${num2}`);
-    //Operacion de resta
-    num1 -= num2;
-    console.log(`La resta es: ${num1}`);
+    //Operacion de Multiplicacion
+    num1 *= num2;
+    console.log(`La Multiplicacion es: ${num1}`);
 
 console.groupEnd();
