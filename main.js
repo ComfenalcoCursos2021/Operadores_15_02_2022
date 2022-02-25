d063d60c6fe924759aa7a5f89cbfdea0266273fa
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-24 19:19:37 -0500

    Operadores Arigmetico Divicion

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Operadores Arigmetico Multiplicacion
+// Operadores Arigmetico Division
 let num1 = parseInt(prompt("Usuario Ingrese un numero mayor que 0", 1));
 let num2 = new Number(prompt("Usuario ingrese siguiente numero", 1));
 let myStyle = `
@@ -8,11 +8,11 @@ let myStyle = `
     border-radius: 5px;
 `;
 
-console.group(`%cOperadores Arigmetico Multiplicacion`, myStyle);
+console.group(`%cOperadores Arigmetico Division`, myStyle);
 
     console.log(`Variable num1: ${num1} num2: ${num2}`);
-    //Operacion de Multiplicacion
-    num1 *= num2;
-    console.log(`La Multiplicacion es: ${num1}`);
+    //Operacion de Division
+    num1 /= num2;
+    console.log(`La Division es: ${num1}`);
 
 console.groupEnd();
