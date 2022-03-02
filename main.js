457dc114a76dc1c48b17ee383af72c7f1ab84b20
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-01 21:27:06 -0500

    Operador logico AND, OR

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
+let a = true;
+let b = false;
+let validar = function(id, ope){
+    document.querySelector(id).insertAdjacentHTML("beforeend",
+    `
+        <tr>
+            <td>${a}</td>
+            <td>${b}</td>
+            <td>${
+                (ope == "&") ? a && b 
+                : (ope == "|") ? a || b 
+                : ""
+            }</td>
+        </tr>
+    `
+    );
+};
+validar("#OperadorAND", "&");
+validar("#OperadorOR", "|");
 
-let validar = data =>{
-    return data ? "TRUE" : "FALSE";
-}
-
-// //Boleanos en objetos, array, arrayAsc
-let dato = ["Miguel"];
-console.log("Boleanos en objetos, array, arrayAsc ", validar(Object.entries(dato).length));
-
-// //Boleanos en numbre
-// 0 = false
-// 1 = true
-console.log("Boleanos en numbre ", validar(0));
-
-// //Boleanos en string
-// "" = false
-// " " = true
-console.log("Boleanos en string ", validar(" "));
-
-
+console.log(!false);
